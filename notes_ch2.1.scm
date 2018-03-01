e2c790dd57163c7178f38951ba98b58eefb11247
Your Name <charlzjin@gmail.com>
2018-02-28 19:47:39 -0500

    Add chapter 2.1 materials

---
+++ notes_ch2.1.scm
@@ -0,0 +1,83 @@
+#lang sicp
+
+#|
+(define (linear-combination a b x y)
+  (+ (* a x) (* b y)))
+
+(define (linear-combination a b x y)
+  (add (mul a x) (mul b y)))
+|#
+
+#|2.1.1 - Example: Arithmetic Operations for Rational Numbers|#
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom y) (numer x))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+(define x (cons 1 2))
+(define y (cons 3 4))
+(define z (cons x y))
+
+#|
+
+> (car (car z))
+1
+
+> (car (cdr z))
+3
+
+; Does not reduce rational numbers to lowest terms:
+
+(define (make-rat n d) (cons n d)) 
+
+|#
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g)
+          (/ d g))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+;(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+;(print-rat (add-rat one-half one-third))
+
+;(print-rat (mul-rat one-half one-third))
+
+;(print-rat
+; (add-rat one-third one-third))
+
